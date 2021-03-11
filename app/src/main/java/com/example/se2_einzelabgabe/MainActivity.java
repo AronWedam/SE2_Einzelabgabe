32d94d36c4c5993f42875aceb1f346c9f5c58430
Aron Wedam <arwedam@edu.aau.at>
2021-03-11 19:54:24 +0100

    implemented - Find Index of Numbers with common divisor - Thread

--- app/src/main/java/com/example/se2_einzelabgabe/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelabgabe/MainActivity.java
@@ -9,8 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.se2_einzelabgabe.ui.main.MainFragment;
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
@@ -19,6 +17,7 @@ import java.net.Socket;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnSend;
+    Button btnFind;
     EditText txt;
     TextView message1;
     TextView message2;
@@ -40,18 +39,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         message2 = findViewById(R.id.message2);
         messageServer = findViewById(R.id.messageServer);
 
-        btnSend = findViewById(R.id.button);
+        btnSend = findViewById(R.id.buttonSend);
+        btnFind = findViewById(R.id.buttonFind);
 
         txt = findViewById(R.id.editNumber);
 
         btnSend.setOnClickListener(this);
+        btnFind.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        String matrikelnr = txt.getText().toString();
-        NetworkThread networkThread = new NetworkThread(matrikelnr);
-        new Thread(networkThread).start();
+        switch (view.getId()) {
+            case R.id.buttonSend:
+                String matrikelnr = txt.getText().toString();
+                NetworkThread networkThread = new NetworkThread(matrikelnr);
+                new Thread(networkThread).start();
+            break;
+            case R.id.buttonFind:
+                matrikelnr = txt.getText().toString();
+                GGTThread ggtThread = new GGTThread(matrikelnr);
+                new Thread(ggtThread).start();
+            break;
+        }
     }
 
     class NetworkThread implements Runnable {
@@ -87,12 +97,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 clientSocked.close();
 
                 mainHandler.post(() -> {
+                    message2.setText("Antwort vom Server");
+                });
+
+                mainHandler.post(() -> {
                     messageServer.setText(modifiedSentence);
                 });
             }
             catch (Exception e){
                 e.printStackTrace();
             }
+        }
+    }
+
+
+    class GGTThread implements Runnable {
+
+        String matrikelnr;
+
+        GGTThread(String matrikelnr) {
+            this.matrikelnr = matrikelnr;
+        }
+
+        @Override
+        public void run() {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < matrikelnr.length(); i++) {
+                int a = Integer.parseInt(matrikelnr.substring(i, i+1));
+                for (int k = i+1; k < matrikelnr.length(); k++) {
+                    int b = Integer.parseInt(matrikelnr.substring(k, k + 1));
+
+                    if(b > a){
+                        for (int j = 2; j < b ; j++) {
+                            if (a % j == 0 && b % j == 0 && a != 0 && b != 0) {
+                                builder.append(i + " ");
+                                builder.append(k + " ");
+                            }
+                        }
+                    }
+
+                    for (int j = 2; j < a; j++) {
+                        if (a % j == 0 && b % j == 0 && a != 0 && b != 0) {
+                            builder.append(i + " ");
+                            builder.append(k + " ");
+                        }
+                    }
+
+                }
+            }
+
+            String text = builder.toString();
+
+            mainHandler.post(() -> {
+                message2.setText("Index der Zahlen mit gleichem Teiler");
+            });
+
+            mainHandler.post(() -> {
+                messageServer.setText(text);
+            });
         }
     }
 }
