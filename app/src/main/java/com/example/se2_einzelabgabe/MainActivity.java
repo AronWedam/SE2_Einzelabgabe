cb542cb8a2c590219bd2fa8fdea1e2b373e88947
Aron Wedam <arwedam@edu.aau.at>
2021-03-11 16:01:36 +0100

    implemented Network Thread

--- app/src/main/java/com/example/se2_einzelabgabe/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelabgabe/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.se2_einzelabgabe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.se2_einzelabgabe.ui.main.MainFragment;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,6 +23,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView message1;
     TextView message2;
     TextView messageServer;
+
+    private Handler mainHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +40,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         message2 = findViewById(R.id.message2);
         messageServer = findViewById(R.id.messageServer);
 
-
         btnSend = findViewById(R.id.button);
 
         txt = findViewById(R.id.editNumber);
@@ -42,6 +49,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        String matrikelnr = txt.getText().toString();
+        NetworkThread networkThread = new NetworkThread(matrikelnr);
+        new Thread(networkThread).start();
+    }
 
+    class NetworkThread implements Runnable {
+
+        private String matrikelnummer;
+        private String sentence;
+        private String modifiedSentence;
+
+        NetworkThread(String matrikelnummer) {
+            this.matrikelnummer = matrikelnummer;
+        }
+
+
+        @Override
+        public void run() {
+            try{
+                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+                Socket clientSocked = new Socket("se2-isys.aau.at", 53212);
+
+
+                DataOutputStream outToServer = new DataOutputStream(clientSocked.getOutputStream());
+
+
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocked.getInputStream()));
+
+                sentence = matrikelnummer;
+
+                outToServer.writeBytes(sentence + '\n');
+
+                modifiedSentence = inFromServer.readLine();
+
+                clientSocked.close();
+
+                mainHandler.post(() -> {
+                    messageServer.setText(modifiedSentence);
+                });
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
