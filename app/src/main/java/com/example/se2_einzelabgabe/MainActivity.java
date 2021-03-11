217028c1b9bbf7cdfdff84d967671630cd468360
Aron Wedam <arwedam@edu.aau.at>
2021-03-11 15:43:54 +0100

    linked GUI elements to MainActivity.java

--- app/src/main/java/com/example/se2_einzelabgabe/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelabgabe/MainActivity.java
@@ -3,19 +3,45 @@ package com.example.se2_einzelabgabe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.se2_einzelabgabe.ui.main.MainFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSend;
+    EditText txt;
+    TextView message1;
+    TextView message2;
+    TextView messageServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
+      /*  if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
-        }
+        } */
+
+        message1 = findViewById(R.id.message1);
+        message2 = findViewById(R.id.message2);
+        messageServer = findViewById(R.id.messageServer);
+
+
+        btnSend = findViewById(R.id.button);
+
+        txt = findViewById(R.id.editNumber);
+
+        btnSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
